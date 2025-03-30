36a533f88e3cc068c5c4ee87168494a907b6b238
Paul Orland <orlandpm@gmail.com>
2025-03-30 09:51:08 -0700

    foo

--- src/linux_synth_install.sh
+++ src/linux_synth_install.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install fluidsynth
+sudo apt install libsdl2-ttf-2.0-0
 
 mkdir -p ./soundfonts
 curl -L -o ./soundfonts/FluidR3_GM.sf2 https://github.com/urish/cppfluid/raw/master/examples/FluidR3_GM.sf2
